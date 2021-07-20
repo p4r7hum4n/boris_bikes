85f02deba7382ed98f47a8d2408a5aa18374309a
AlexiaConstant <alexia.constant@yahoo.com>
2021-07-20 17:10:24 +0100

    Day 2 part 4

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,14 @@
 require_relative "bike"
 
 class DockingStation
+  attr_reader :bike
+
   def release_bike 
-    Bike.new
+    @bike
   end
 
   def dock(bike)
-    bike
+    @bike = bike
   end
-  
-  def bike
-  end  
+
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -9,30 +9,40 @@ describe DockingStation do
     end
   end
 
+  describe '#release_bike' do
+    it 'releases a bike' do
+      bike = Bike.new
+      subject.dock(bike)
+      expect(subject.release_bike).to eql bike
+    end
+  end
+=begin
   describe 'gets bike' do 
     it 'gets a bike' do
     docking_station = DockingStation.new
     expect(docking_station.release_bike).to respond_to(:working?)
     end
   end
-
+=end 
   describe "dock bike" do
     it { is_expected.to respond_to(:dock).with(1).argument } 
   end  
 
-  describe "bike in docking station"
+  describe "bike in docking station" do
     it { is_expected.to respond_to(:bike) }
+  end
 
   describe "docks a bike" do
     it 'dock a bike new' do
       bike = Bike.new
       expect(subject.dock(bike)).to eql (bike)
     end
+  end
 
+  describe 'return docked bike' do
     it 'return docked bikes' do
       bike = Bike.new
-      expect(subject.bike).to eql (bike)
+      expect(subject.bike).to eql (@bike)
     end
-
   end
 end
